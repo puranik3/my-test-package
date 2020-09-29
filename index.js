4144256514a9180e9a983b9f577375ffe9b868b9
Prashanth Puranik <puranik@digdeeper.in>
2020-09-29 19:55:44 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+import Alert from './src/Alert';
+import Pagination from './src/Pagination';
+import VotingWidget from './src/VotingWidget';
+
+export {
+    Alert,
+    Pagination,
+    VotingWidget
+};
